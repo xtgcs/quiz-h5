096513c094330cc6bbbd8e354f55712007f5aed0
xtgcs <495035242@qq.com>
2019-04-16 20:30:54 +0800

    修改页面标题

--- src/router.js
+++ src/router.js
@@ -4,34 +4,58 @@ import Home from './views/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router =  new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      meta: {
+        title: '谁是少数派？瓜分积分换大奖！'
+    }
     },
     {
       path: '/detail',
       name: 'detail',
-      component: () => import(/* webpackChunkName: "about" */ './views/detail.vue')
+      component: () => import(/* webpackChunkName: "about" */ './views/detail.vue'),
+      meta: {
+        title: '竞猜详情'
+    }
     },
     {
       path: '/prizeList',
       name: 'prizeList',
-      component: () => import(/* webpackChunkName: "about" */ './views/prizeList.vue')
+      component: () => import(/* webpackChunkName: "about" */ './views/prizeList.vue'),
+      meta: {
+        title: '往期记录'
+    }
     },
     {
       path: '/record',
       name: 'record',
-      component: () => import(/* webpackChunkName: "about" */ './views/record.vue')
+      component: () => import(/* webpackChunkName: "about" */ './views/record.vue'),
+      meta: {
+        title: '首页'
+    }
     },
     {
       path: '/quizRecord',
       name: 'quizRecord',
-      component: () => import(/* webpackChunkName: "about" */ './views/quizRecord.vue')
+      component: () => import(/* webpackChunkName: "about" */ './views/quizRecord.vue'),
+      meta: {
+        title: '竞猜记录'
+    }
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {//beforeEach是router的钩子函数，在进入路由前执行
+  if (to.meta.title) {//判断是否有标题
+    document.title = to.meta.title
+  }
+  next()//执行进入路由，如果不写就不会进入目标页
+})
+
+export default router
