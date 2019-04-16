1ef09a1244c311087e10fe1e86d3d8920864c31e
lilong <lilong@missfresh.cn>
2019-04-16 20:32:44 +0800

    local

--- src/views/detail.vue
+++ src/views/detail.vue
@@ -50,7 +50,7 @@
       <div class="modal__box">
         <div>
           <label for="phone__num">手机号</label>
-          <input class="phone__number__input" placeholder="请输入手机号" type="number">
+          <input v-model="mobile_num" id="phone__num" class="phone__number__input" placeholder="请输入手机号" type="number">
         </div>
         <div class="phone-tip">请填写手机号，活动结束后，积分将通过线下方式发送到您的手机号</div>
         <!-- small-btn -->
@@ -76,7 +76,9 @@ export default {
 			select_left: false,
 			select_right: false,
 			select_value: '',
-			mobile_num: ''
+      mobile_num: '',
+      res_options__key: "",
+      res_options__value: ""
     };
   },
   components: {
@@ -90,9 +92,6 @@ export default {
   },
 
   methods: {
-		confirmProject() {
-			// confirm("focus/save", "POST", {"focus_id": parseInt(this.focus_id), "mobile": this.mobile_num, "option_number": })
-		},
     initPage() {
 			console.log({'focus_id': parseInt(this.focus_id)})
       projectDetail("focus/detail", "", {'focus_id': parseInt(this.focus_id)}).then(res => {
@@ -100,17 +99,25 @@ export default {
         if (respones.code == 0) {
 					let reg = new RegExp("\n","g")
 					let str = respones.data.memo.replace(reg, "<br>");
-					this.gussDetail = respones.data;
+          this.gussDetail = respones.data;
 					this.gussDetail.memo = str
         }
       });
 		},
-		click_left() {
+		click_left(options) {
+      const key = Object.keys(options)
+      this.res_options__key = key[0]
+      this.res_options__value = options[this.res_options__key]
+
 			this.select_value = 'left'
 			this.select_left = true
 			this.select_right = false
 		},
-		click_right() {
+		click_right(options) {
+      const key = Object.keys(options)
+      this.res_options__key = key[0]
+      this.res_options__value = options[this.res_options__key]
+
 			this.select_value = 'right'
 			this.select_right = true
 			this.select_left = false
@@ -119,6 +126,7 @@ export default {
 			if (this.select_value == '') {
 				alert("请选择你的观点")
 			} else {
+        this.mobile_num = ""
 				this.show__rule = true;
 			}
     },
@@ -126,7 +134,18 @@ export default {
       this.show__rule = false;
     },
     confirmJoin() {
+      if (!this.mobile_num) {
+        alert("请输入手机号")
+        return
+      }
       // request ajax
+      confirm("focus/save", "POST", {"focus_id": parseInt(this.focus_id), "mobile": this.mobile_num, "option_number": this.res_options__key, "my_result": this.res_options__value}).then(res => {
+        if (res.code == 0) {
+          
+        } else {
+          alert(res.msg || '网络错误，请稍后重试')
+        }
+      })
       this.show__rule = false;
     }
   }
